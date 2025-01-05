f91657002dbe1738b3d3918a09f838b0f722f480
Tatare <tazet@narod.ru>
2025-01-05 08:59:18 +0300

    2

--- SQL/pg.go
+++ SQL/pg.go
@@ -1,39 +1,45 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"os"
 
-	"github.com/jackc/pgx/v5"
+	"database/sql"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 func main() {
-	ctx := context.Background()
-	//url := "postgres://postgres:passwordas@localhost:5432/postgres"
-	url := "postgres://postgres:passwordas@forgo.c7wegmiakpkw.us-west-1.rds.amazonaws.com:5432/postgres"
-
+	//	ctx := context.Background()
+	url := os.Getenv("DATABASE_DSN")
+	
+	url, _ = os.LookupEnv("DATABASE_DSN")
+	
+	url = "postgres://postgres:passwordas@forgo.c7wegmiakpkw.us-west-1.rds.amazonaws.com:5432/postgres"
+	//url = "postgres://postgres:passwordas@localhost:5432/postgres"
 	//	postgres://postgres:mypassword@rds-postgres.xxxxx.amazonaws.com:5432
 	//	postgres://postgres:zalupa77@rds-postgres.xxxxx.amazonaws.com:5432
 
-	conn, err := pgx.Connect(ctx, url)
+	db, err := sql.Open("pgx", url)
+
+	//	conn, err := pgx.Connect(ctx, url)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
-	defer conn.Close(ctx)
+	defer db.Close()
 
-	perr := conn.Ping(ctx)
-	log.Printf("perr %v\n conn type %T\n value %v\n", perr, conn, conn)
+	perr := db.Ping()
+	log.Printf("perr %v\n conn type %T\n value %v\n", perr, db, db)
 
-	var name string
-	var weight int64
-	err = conn.QueryRow(context.Background(), "select name, weight from widgets where id=$1", 42).Scan(&name, &weight)
-	if err != nil {
-		//		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
-		os.Exit(1)
-	}
+	// var name string
+	// var weight int64
+	// err = conn.QueryRow(context.Background(), "select name, weight from widgets where id=$1", 42).Scan(&name, &weight)
+	// if err != nil {
+	// 	//		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+	// 	os.Exit(1)
+	// }
 
-	fmt.Println(name, weight)
+	// fmt.Println(name, weight)
 }
