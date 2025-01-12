4af2f64a0dfed98fb9798f1c35fc0e456d218f8f
Naeel <tazet@narod.ru>
2025-01-12 19:03:12 +0300

    5

--- SQL/pg.go
+++ SQL/pg.go
@@ -20,7 +20,7 @@ func main() {
 	url, _ = os.LookupEnv("DATABASE_DSN")
 
 	//url = "postgres://postgres:passwordas@forgo.c7wegmiakpkw.us-west-1.rds.amazonaws.com:5432/forgo"
-	url = "postgres://postgres:passwordas@localhost:5432/forgot"
+	url = "postgres://postgres:passwordasa@localhost:5432/forgo"
 	//	postgres://postgres:mypassword@rds-postgres.xxxxx.amazonaws.com:5432
 	//	postgres://postgres:zalupa77@rds-postgres.xxxxx.amazonaws.com:5432
 
@@ -32,7 +32,7 @@ func main() {
 		var pgErr *pgconn.PgError
 		if errors.As(err, &pgErr) {
 			log.Printf("err code is %v", pgErr.Code)
-			pgErr.
+			//pgErr.
 		}
 
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
