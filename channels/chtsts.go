8e78f85ee1b850c558660839e31f4b4bba7a9329
Tatare <tazet@narod.ru>
2025-05-03 16:02:55 +0400

    serie

--- channels/chtsts.go
+++ channels/chtsts.go
@@ -9,16 +9,16 @@ func main() {
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-//	cancel()
+	//	cancel()
 
 	stop := make(chan int)
 	//	go func() {
-	cha := generatoras(ctx, 5)
+	cha := generateInts(ctx, 5)
 	//	}()
 	go func() {
 		for c := range cha {
 			fmt.Println("-->> read cha ", c)
-			if c == 0 {
+			if c == 1 {
 				cancel()
 			}
 		}
@@ -35,20 +35,34 @@ func main() {
 
 }
 
-func generatoras(ctx context.Context, n int) (cha chan int) {
+func generateInts(ctx context.Context, n int) (cha chan int) {
 	cha = make(chan int)
 	go func() {
 		defer close(cha)
 		for i := range n {
 			select {
+			case cha <- i:
+				fmt.Println("Generate ", i)
 			case <-ctx.Done():
 				fmt.Println("cancel ON  ", i)
 				return
-			default:
-				fmt.Println("Generate ", i)
-				cha <- i
 			}
 		}
 	}()
+	return
+}
+
+func duplicateChannels(ctx context.Context, in chan int, n int) (out []chan int) {
+	serie := 0
+	for c := range in {
+		for range n {
+			if serie < n {
+				cha := make(chan int)
+				out = append(out, cha)
+			}
+			out[serie/n] <- c
+			serie++
+		}
+	}
 	return
 }
